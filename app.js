df322b61910bbb8cee215df8e4a483fd1f97370d
tree70373 <tree70373@gmail.com>
2023-02-16 22:43:14 +0800

    git init

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const routes = require('./routes')
+require('./config/mongoose')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
